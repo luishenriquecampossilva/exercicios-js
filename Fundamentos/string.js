9b5281e876125e5f91db367ae30939c682188d57
luis <luiscampossilva96@gmail.com>
2020-03-22 21:12:37 -0300

    subindo string.js

---
+++ Fundamentos/string.js
@@ -0,0 +1,12 @@
+const escola = "Cad3r"
+
+console.log(escola.charAt(4))///a função charAt renorta a letra que se encontra na posição 4 da constante escola
+console.log(escola.charAt(5))///Nesse caso retornaria um valor vazio
+console.log(escola.charCodeAt(3))//retorna o valor de um elemento de uma string na tabela ask ou na tabela unicode
+console.log(escola.indexOf('3'))//retorna em que posição se encontra o valor 3
+console.log(escola.substring(1))//retorna um valor a parti de uma deterninada posicao em uma string
+console.log(escola.substring(0,3))//retorna a partir do indice 0 ate o indice 3 sem incluir o indice 3
+console.log('escola '.concat(escola).concat(" !"))//concatena elementos
+console.log(escola.replace(3,'e'))//Substitui um valor pelo outro
+console.log("Ana,Mari,Pedro".split(','))//Convert uma string em uma rray passando um delimitador
+///String e uma cadeia de caracteres///
