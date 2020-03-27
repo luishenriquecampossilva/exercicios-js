7087f350029702723e25c03865c6a99c5418fed3
luis <luiscampossilva96@gmail.com>
2020-03-26 21:16:31 -0300

    subindo novos arquivos

---
+++ Fundamentos/uasandovar2.js
@@ -0,0 +1,12 @@
+
+
+
+var numero = 1
+
+{
+
+    var numero = 2
+    console.log('dentro = ',numero)
+}
+
+console.log('fora=',numero)
---
+++ Fundamentos/usandolet1.js
@@ -0,0 +1,14 @@
+let numero = 1
+
+{
+
+
+    let numero = 2 //variaveis do tipo let so estão acessiveis no mesmo bloco
+    console.log('dentro = ',numero)
+}
+
+console.log('fora =',numero)
+
+///variaveis dfinias com a palavra reservada tem escopo global e escopo de função
+///variaveis definidas com a palavra reservada let tem escopo global escopo de função e escopo de bloco
+
