00e12b888886c8cc1605b47c49eba71995eb5e29
luis henrique campos silva <luiscampossilva96@gmail.com>
2020-03-30 14:43:49 -0300

    subindo arquivos

---
+++ Fundamentos/operadordestructuring.js/operadoresAritmeticos.js
@@ -0,0 +1,15 @@
+const [a,b,c,d] = [3,4,1,5]
+
+const soma = a+b+c+d
+
+
+
+const subtracao = d-c
+
+const multiplicacao = a*b
+const divisao = d/a
+const modulo = a%2
+
+console.log(soma,subtracao,modulo,multiplicacao,divisao)
+
+//operador unario tem um unico operando//
